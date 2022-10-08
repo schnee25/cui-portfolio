994096fae24810a4af44d60dcd5a698ea9ad6dea
schnee25 <schnee.2114@gmail.com>
2022-10-08 23:33:50 +0900

    環境構築Done

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div>
+          <h1>Hello TS-React!</h1>
+          <h1>皆さん、こんにちは</h1>
+          <blockquote>
+            <p>2022/10/08</p>
+          </blockquote>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
