a8fea18cdfc6908530d7abad1e16fd809acc3104
schnee25 <schnee.2114@gmail.com>
2022-10-09 16:10:31 +0900

    catコマンド作成

---
+++ src/components/command/cat/Cat.tsx
@@ -0,0 +1,65 @@
+import { FC } from "react";
+import NoFileOrDir from "../error/NoFileOrDir";
+import IsDir from "../error/IsDir";
+
+import { CAT_FILE_CONTENTS } from "../../../util";
+import React from "react";
+
+type Props = {
+  dirItem: string[];
+  fileName: string;
+  currentDir: string;
+  isFormatted: boolean;
+};
+
+const Cat: FC<Props> = ({ dirItem, fileName, currentDir, isFormatted }) => {
+  // フォルダにcatを実行した場合にエラーを返す
+  if (!fileName.includes(".")) {
+    return <IsDir fileName={fileName} />;
+  }
+
+  // catでwhite関連のファイルの場合タグで囲んだファイルの中身を返す
+  else if (dirItem.includes(fileName) && currentDir === WHITE_PATH) {
+    if (fileName === "README.md") {
+      return WHITE_README_FILE_CONTENT;
+    } else if (fileName === "requirements.txt") {
+      return REQUIREMENTS_FILE_CONTENT;
+    } else if (fileName === "main.py0") {
+      return isFormatted ? MAIN_FILE_CONTENT_SEMI : MAIN_FILE_CONTENT;
+    } else if (fileName === "pyproject.toml") {
+      return PYPROJECT_FILE_CONTENT;
+    }
+  }
+
+  // catでslide関連のファイルの場合エラーを返す
+  else if (dirItem.includes(fileName) && currentDir === SLIDE_PATH) {
+    if (fileName === "README.md") {
+      return SLIDE_README_FILE_CONTENT;
+    } else if (fileName.includes("pptx")) {
+      return <IsSlide fileName={fileName} />;
+    }
+  }
+
+  // white関連のファイルでないならdirItemに含まれるCAT_FILE_CONTENTSの値を表示
+  else if (dirItem.includes(fileName)) {
+    // urlがないならhover時にunderlineをつけない
+    return CAT_FILE_CONTENTS[fileName].url === undefined ? (
+      <p>{CAT_FILE_CONTENTS[fileName].content}</p>
+    ) : (
+      <p className="font-ubuntu_terminal text-white">
+        <a
+          className="hover:underline"
+          href={CAT_FILE_CONTENTS[fileName].url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {CAT_FILE_CONTENTS[fileName].content}
+        </a>
+      </p>
+    );
+  } else {
+    return <NoFileOrDir command={"cat"} fileOrDir={fileName} />;
+  }
+};
+
+export default Cat;
--- src/util.tsx
+++ src/util.tsx
@@ -1,11 +1,12 @@
+import { calcAge } from "./CalcAge";
 export const HOME_PATH: string = "/home";
 export const YUKI_PATH: string = "/home/yuki";
 export const PRODUCTS_PATH: string = "/home/yuki/products";
 export const CONTACTS_PATH: string = "/home/yuki/contacts";
 export const LS_HOME_ITEM: string[] = ["yuki"];
-
 export const LS_YUKI_ITEM: string[] = ["profile.txt", "products", "contacts"];
 export const LS_PRODUCTS_ITEM: string[] = ["portfolio.txt", "cui-portfolio.txt"];
+
 export const LS_CONTACTS_ITEM: string[] = [
   "twitter.txt",
   "github.txt",
@@ -14,3 +15,33 @@ export const LS_CONTACTS_ITEM: string[] = [
   "qiita.txt",
   "gmail.txt",
 ];
+
+export const CAT_FILE_CONTENTS = {
+  "profile.txt": {
+    content:
+      "Name: yuki(こひまめ), Age:" +
+      calcAge(20001226).toString() +
+      " , Likes: 美味しいものを食べること",
+  },
+
+  "twitter.txt": {
+    content: "twitter: こひまめ ",
+    url: "https://twitter.com/schnee25v",
+  },
+  "github.txt": {
+    content: "GitHub: schnee25",
+    url: "https://github.com/schnee25",
+  },
+  "facebook.txt": {
+    content: "facebook: Yukino Oyamada",
+    url: "https://www.facebook.com/yuki.O2525/",
+  },
+  "instagram.txt": {
+    content: "instagram:nemunemunyanko_chan ",
+    url: "https://www.instagram.com/nemunemunyanko_chan/",
+  },
+  "gmail.txt": {
+    content: "Gmail:yukino.oyamada@gmail.com",
+    url: "mailto:yukino.oyamada@gmail.com",
+  },
+};
